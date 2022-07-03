4ecee7564e036884bb9f74f7bd95d1e3691a608e
LE TRUNG HIEN <108418838+trunghienjav@users.noreply.github.com>
2022-07-03 22:24:15 +0900

    Create form_insert.php

---
+++ form_insert.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title></title>
+</head>
+<body>
+<form method="post" action="process_insert.php">
+	Tiêu đề
+	<input type="text" name="tieu_de" value="">
+	<br>
+	Nội dung
+	<textarea name="noi_dung" value=""></textarea>
+	<br>
+	Link ảnh
+	<input type="text" name="anh" value="">
+	<br>
+	<button>Thêm</button>
+</form>
+</body>
+</html>
